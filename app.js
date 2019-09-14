0c89d0bbbc528db5ed58ed203d0180b0f132301c
8abc <anadelaine.catalla@gmail.com>
2019-09-14 14:37:20 -0700

    created basic todo list

---
+++ app.js
@@ -0,0 +1,18 @@
+let todos = ["clean"];
+
+let input = prompt("What would you like to do?");
+while (input !== "quit") {
+  //handle input
+  if (input === "list") {
+    console.log(todos);
+  } else if (input === "new") {
+    //ask for new todo
+    let newTodo = prompt("Enter new todo");
+    //add todos array
+    todos.push(newTodo);
+  }
+  //ask again for new input
+  input = prompt("What would you like to do?");
+}
+
+console.log("OK, you quit the app");
