ee256d4f01aede1192f1edc98581d9f318cd224e
8abc <anadelaine.catalla@gmail.com>
2019-10-29 20:56:40 -0700

    added icons and animation

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,10 +1,12 @@
 // Check Off Specific Todos by clicks
-$("li").click(function() {
+//Adds click listener to entire ul and li that's in ul
+$("ul").on("click", "li", function() {
   $(this).toggleClass("completed");
 });
 
 //Click on X to delete Todo
-$("span").click(function(event) {
+//adds event listeners to all elements on the page
+$("ul").on("click", "span", function(event) {
   $(this)
     //removes the <li> in the span
     .parent()
@@ -15,4 +17,16 @@ $("span").click(function(event) {
     });
   //stops the other alerts from popping up
   event.stopPropagation();
+});
+
+//13 code for enter key
+$("input[type='text']").keypress(function(event) {
+  if (event.which === 13) {
+    //grabs new todo text from input
+    var todoText = $(this).val();
+    //clears the input box
+    $(this).val("");
+    //create a new li and add to ul
+    $("ul").append("<li><span>X</span> " + todoText + " </li>");
+  }
 });
