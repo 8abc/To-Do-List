61a67ab827f6d1da890fad81aef16bbdfffa7246
8abc <anadelaine.catalla@gmail.com>
2019-11-06 16:53:10 -0800

    created checkbox component with bootstrap

---
+++ react/to-do/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Checkbox from "./components/Checkbox";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Checkbox />
+    </div>
+  );
+}
+
+export default App;
