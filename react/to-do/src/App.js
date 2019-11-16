453e40e47a6bdfd6c867e7215641f75ee00d10fa
8abc <anadelaine.catalla@gmail.com>
2019-11-16 14:08:03 -0800

    added handleChang and setState to the checkbox

--- react/to-do/src/App.js
+++ react/to-do/src/App.js
@@ -12,13 +12,30 @@ class App extends React.Component {
     this.state = {
       todos: todosData
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    //prevState mirrors our state from line 12
+    this.setState(prevState => {
+      const updatedTodos = prevState.todos.map(todo => {
+        //if an item that matches an id is clicked there will be a checkbox
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      });
+      return {
+        todos: updatedTodos
+      };
+    });
   }
 
   render() {
     //new array to map method
     //we map through todosData is in state
     const todoItems = this.state.todos.map(item => (
-      <TodoItem key={item.id} item={item} />
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
     ));
     return (
       <div className="todo-list">
